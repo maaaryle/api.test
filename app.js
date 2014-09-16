29cce4415c4b337bd756acbce458d1c9ff454ac3
Samer Buna <samer.buna@gmail.com>
2014-09-16 15:01:27 -0700

    Prep for twitter search

--- app.js
+++ app.js
@@ -61,20 +61,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-var Twit = require('twit');
-
-var T = new Twit({
-  consumer_key: process.env.CONSUMER_KEY
-  , consumer_secret: process.env.CONSUMER_SECRET
-  , access_token: process.env.ACCESS_TOKEN
-  , access_token_secret: process.env.ACCESS_TOKEN_SECRET
-});
-
-//  tweet 'hello world!'
-//
-T.post('statuses/update', { status: 'Hello API! #codinghouse-phase2' }, function(err, data, response) {
-  console.log(data);
-});
-
-
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,22 @@ router.get('/twitter', function(req, res) {
   res.render('twitter');
 });
 
+var Twit = require('twit');
+
+var T = new Twit({
+  consumer_key: process.env.CONSUMER_KEY
+  , consumer_secret: process.env.CONSUMER_SECRET
+  , access_token: process.env.ACCESS_TOKEN
+  , access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+
+router.post('/twitter', function(req, res) {
+  // req.body.tweet
+  T.post('statuses/update', { status: req.body.tweet }, function(err, data, response) {
+    console.log(data);
+  });
+  res.redirect("/twitter");
+});
+
 module.exports = router;
